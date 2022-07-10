015da35a1cc0b55a9d3ad6daea2d9734070209fa
vemolista <24554998+vemolista@users.noreply.github.com>
2022-07-10 12:50:09 +0200

    Clean up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import { ChakraProvider, Box, theme } from "@chakra-ui/react";
-import { digitalManagement, informationSystems } from "./declarations/data";
-import { ProgrammeLevel } from "./declarations/types";
-import { showRewards } from "./components/showRewards";
-import { RequirementsBox } from "./components/requirementsBox";
 
 export const App = () => (
 	<ChakraProvider theme={theme}>
 		<Box padding={10} fontSize="xl" style={{ border: "3px solid black" }}>
-			<Box>
-				{ProgrammeLevel[digitalManagement.programmeLevel]}:{" "}
-				{digitalManagement.title}
-			</Box>
-			{showRewards(digitalManagement.mandatoryCourses)}
-			<Box>
-				<Box>
-					{`${ProgrammeLevel[informationSystems.programmeLevel]}: ${
-						informationSystems.title
-					}`}
-				</Box>
-				<RequirementsBox requirements={informationSystems.requirements} />
+			<Box textAlign={"center"} fontSize={"3xl"}>
+				The most beautiful pathfinder (there is literally no other)
 			</Box>
 		</Box>
 	</ChakraProvider>
