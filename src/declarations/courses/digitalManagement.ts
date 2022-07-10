f7c178bd67e697f60097bcd47177551afc7ead37
vemolista <24554998+vemolista@users.noreply.github.com>
2022-07-10 17:50:50 +0200

    Create a folder for courses

---
+++ src/declarations/courses/digitalManagement.ts
@@ -0,0 +1,72 @@
+import { Area, Course } from "../types";
+
+export const BA_BDMAO1002U: Course = {
+	code: "BA_BDMAO1002U",
+	title: "Digital Technologies and Data-Driven Business",
+	rewards: [
+		{
+			area: Area.IT,
+			creditsInECTS: 5,
+		},
+		{
+			area: Area["Business Administration"],
+			creditsInECTS: 10,
+		},
+	],
+};
+
+export const BA_BDMAO1003U;
+
+// [
+//     {
+//         code: "DM0001",
+//         title: "Digital Technologies and Data-Driven Business",
+//         rewards: [
+//             {
+//                 area: Area.IT,
+//                 creditsInECTS: 5,
+//             },
+//             {
+//                 area: Area["Business Administration"],
+//                 creditsInECTS: 10,
+//             },
+//         ],
+//     },
+//     {
+//         code: "DM0001",
+//         title: "Business Data Analytics, Quantitative Methods and Visualisation",
+//         rewards: [
+//             {
+//                 area: Area.IT,
+//                 creditsInECTS: 10,
+//             },
+//             {
+//                 area: Area["Business Administration"],
+//                 creditsInECTS: 5,
+//             },
+//         ],
+//     },
+//     {
+//         code: "DM0001",
+//         title: "Economics and Strategy in the Digital Age",
+//         rewards: [
+//             {
+//                 area: Area.Microeconomics,
+//                 creditsInECTS: 7.5,
+//             },
+//         ],
+//     },
+//     {
+//         code: "DM0002",
+//         title: "Managing Marketing, Communications and Relations",
+//         rewards: [
+//             {
+//                 area: Area.Marketing,
+//                 creditsInECTS: 7.5,
+//             },
+//             {
+//                 area: Area["Business Administration"],
+//                 creditsInECTS: 7.5,
+//             },
+//         ],
+//     },
