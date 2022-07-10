be6ee44443dd7d276b5e1b718888da274db32d98
vemolista <24554998+vemolista@users.noreply.github.com>
2022-07-10 18:17:56 +0200

    Split digital management courses to another file

--- src/declarations/courses/digitalManagement.ts
+++ src/declarations/courses/digitalManagement.ts
@@ -1,7 +1,7 @@
 import { Area, Course } from "../types";
 
 export const BA_BDMAO1002U: Course = {
-	code: "BA_BDMAO1002U",
+	code: "BA-BDMAO1002U",
 	title: "Digital Technologies and Data-Driven Business",
 	rewards: [
 		{
@@ -15,58 +15,43 @@ export const BA_BDMAO1002U: Course = {
 	],
 };
 
-export const BA_BDMAO1003U;
+export const BA_BDMAO1022U: Course = {
+	code: "BA-BDMAO1022U",
+	title: "Managing Communication, Marketing and Relations",
+	rewards: [
+		{
+			area: Area["Business Administration"],
+			creditsInECTS: 5,
+		},
+		{
+			area: Area.Marketing,
+			creditsInECTS: 10,
+		},
+	],
+};
 
-// [
-//     {
-//         code: "DM0001",
-//         title: "Digital Technologies and Data-Driven Business",
-//         rewards: [
-//             {
-//                 area: Area.IT,
-//                 creditsInECTS: 5,
-//             },
-//             {
-//                 area: Area["Business Administration"],
-//                 creditsInECTS: 10,
-//             },
-//         ],
-//     },
-//     {
-//         code: "DM0001",
-//         title: "Business Data Analytics, Quantitative Methods and Visualisation",
-//         rewards: [
-//             {
-//                 area: Area.IT,
-//                 creditsInECTS: 10,
-//             },
-//             {
-//                 area: Area["Business Administration"],
-//                 creditsInECTS: 5,
-//             },
-//         ],
-//     },
-//     {
-//         code: "DM0001",
-//         title: "Economics and Strategy in the Digital Age",
-//         rewards: [
-//             {
-//                 area: Area.Microeconomics,
-//                 creditsInECTS: 7.5,
-//             },
-//         ],
-//     },
-//     {
-//         code: "DM0002",
-//         title: "Managing Marketing, Communications and Relations",
-//         rewards: [
-//             {
-//                 area: Area.Marketing,
-//                 creditsInECTS: 7.5,
-//             },
-//             {
-//                 area: Area["Business Administration"],
-//                 creditsInECTS: 7.5,
-//             },
-//         ],
-//     },
+export const BA_BDMAO1023U: Course = {
+	code: "BA-BDMAO1023U",
+	title: "Business Data Analytics, Quantitative Methods and Visualization",
+	rewards: [
+		{
+			area: Area["Business Administration"],
+			creditsInECTS: 4,
+		},
+		{
+			area: Area.IT,
+			creditsInECTS: 3.5,
+		},
+	],
+};
+
+export const BA_BDMAO1024U: Course = {
+	code: "BA-BDMAO1024U",
+	title: "Economics in the Digital Age",
+	rewards: [
+		{
+			area: Area.Microeconomics,
+			creditsInECTS: 7.5,
+		},
+	],
+};
