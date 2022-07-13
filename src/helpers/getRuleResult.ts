fb4e8130fbd8e1d060ea9d08534997d91a2b2547
vemolista <24554998+vemolista@users.noreply.github.com>
2022-07-13 16:59:14 +0200

    Refactor function to get rule results

---
+++ src/helpers/getRuleResult.ts
@@ -0,0 +1,13 @@
+import { Rule, RuleResult, Engine } from "json-rules-engine";
+
+export const getRuleResult = async (
+	rule: Rule,
+	fact: Record<string, number>
+) => {
+	const engine = new Engine();
+
+	return await engine
+		.addRule(rule)
+		.run(fact)
+		.then((result) => [...result.results, ...result.failureResults]);
+};
