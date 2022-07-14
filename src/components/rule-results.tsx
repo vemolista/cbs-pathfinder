cde720975a837daa22399238fac31633f1e20b92
vemolista <24554998+vemolista@users.noreply.github.com>
2022-07-14 13:03:41 +0200

    Split SingleRuleResult into separate file

--- src/components/rule-results.tsx
+++ src/components/rule-results.tsx
@@ -37,32 +37,3 @@ export const RuleResults = (props: RuleResultsProps) => {
 		</Box>
 	);
 };
-
-interface SingleRuleResultProps {
-	ruleResult: {
-		result: boolean;
-		fact: string;
-		value: number;
-		operator: string;
-		factResult: number;
-	};
-}
-
-const SingleRuleResult = (props: SingleRuleResultProps) => {
-	const { result, fact, value, operator, factResult } = props.ruleResult;
-
-	return (
-		<ListItem
-			border={`2px solid ${result ? "green" : "red"}`}
-			backgroundColor={result ? "lightgreen" : "lightsalmon"}
-		>
-			<HStack>
-				<Box>{result ? "fulfilled" : "not fulfilled"}</Box>
-				<Box>{fact}</Box>
-				<Box>{value}</Box>
-				<Box>{operator}</Box>
-				<Box>{factResult}</Box>
-			</HStack>
-		</ListItem>
-	);
-};
