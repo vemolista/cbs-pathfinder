3913d5dedebf808781445e2d0188244661fa1a6c
vemolista <24554998+vemolista@users.noreply.github.com>
2022-07-31 12:06:26 +0200

    Show error when no programme is selected

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import {
 	Heading,
 	Button,
 	ButtonGroup,
+	Alert,
+	AlertIcon,
+	AlertTitle,
+	VStack,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CourseList } from "./components/course-list";
 import { GraduateProgrammes } from "./components/graduate-programmes";
 import { ProgrammeList } from "./components/programme-list";
@@ -22,7 +26,13 @@ export const App = () => {
 	const [step, setStep] = useState<Step>(Step.InputProgramme);
 	const [passedCourses, setPassedCourses] = useState<Course[]>([]);
 	const [currentProgramme, setCurrentProgramme] = useState<Programme>();
+	const [error, setError] = useState("");
+
 	let stepComponent;
+
+	useEffect(() => {
+		if (currentProgramme) setError("");
+	}, [currentProgramme]);
 
 	switch (step) {
 		case Step.InputProgramme:
@@ -48,21 +58,29 @@ export const App = () => {
 			stepComponent = <Box>something went wrong, yikes</Box>;
 	}
 
+	const handleNext = () => {
+		if (step === Step.InputProgramme && !currentProgramme)
+			return setError("Select a programme");
+		setStep(step + 1);
+	};
+
 	return (
 		<ChakraProvider theme={theme}>
 			<Box padding={10}>
 				<Heading as={"h1"} size={"2xl"} textAlign={"center"} margin={10}>
 					Pathfinder
 				</Heading>
-				<Box
+				<VStack
 					marginBottom={5}
+					spacing={2}
 					padding={5}
 					border={"1px solid lightgray"}
 					borderRadius={"lg"}
+					alignItems={"flex-start"}
 				>
 					<ButtonGroup spacing={5}>
 						{step < Object.keys(Step).length / 2 - 1 && (
-							<Button onClick={() => setStep(step + 1)}>Next</Button>
+							<Button onClick={() => handleNext()}>Next</Button>
 						)}
 						{step > 0 && (
 							<Button onClick={() => setStep(step - 1)}>Back</Button>
@@ -77,7 +95,13 @@ export const App = () => {
 							</Button>
 						)}
 					</ButtonGroup>
-				</Box>
+					{error && (
+						<Alert status="error">
+							<AlertIcon />
+							<AlertTitle>{error}</AlertTitle>
+						</Alert>
+					)}
+				</VStack>
 				{stepComponent}
 			</Box>
 		</ChakraProvider>
